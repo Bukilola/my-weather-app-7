76e2c6ba7bb00c230b4c670edb7be02ee2e12da7
Bukilola <Arasanyin01@gmail.com>
2022-10-26 15:19:19 +0200

    updates going on

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+     let cityElement = document.querySelector("#city");
+     let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);  
+  cityElement.innerHTML = response.data.name; 
+   descriptionElement.innerHTML = response.data.weather[0].description; 
+}
+
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Weisendorf &appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
