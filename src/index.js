49e6f4e694d808ffee9385d80e23b4d82ec5e3b0
Bukilola <Arasanyin01@gmail.com>
2022-10-31 14:46:05 +0100

    Working

--- src/index.js
+++ src/index.js
@@ -29,7 +29,9 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+  
   celsiusTemperature = response.data.main.temp;
+ 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
